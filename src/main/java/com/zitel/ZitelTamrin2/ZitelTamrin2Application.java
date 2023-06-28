f82ac942c2866e0145ecf88929aeb5c1acc2c441
OmidMerikhi <omidi.merikhi1999@gmail.com>
2023-06-28 14:44:32 +0330

    create project and share on github

---
+++ src/main/java/com/zitel/ZitelTamrin2/ZitelTamrin2Application.java
@@ -0,0 +1,13 @@
+package com.zitel.ZitelTamrin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZitelTamrin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZitelTamrin2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zitel/ZitelTamrin2/ZitelTamrin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zitel.ZitelTamrin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZitelTamrin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
